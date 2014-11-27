7fe0bb332d6a881efcc8bf772956480217351f83
Cameron Irmas <cjirmas@gmail.com>
2014-11-27 08:56:22 -0800

    users can see all drinks

--- app/controllers/drinks_controller.rb
+++ app/controllers/drinks_controller.rb
@@ -1,2 +1,42 @@
 class DrinksController < ApplicationController
+  before_action :set_drink, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @drinks = Drink.all
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+    @drink = @drink.update(drink_params)
+    redirect_to drink_path(@drink)
+  end
+
+  def destroy
+    @drink.destroy
+    redirect_to drinks_path
+  end
+
+  private
+
+  def set_drink
+    @drink = Drink.find(params[:id])
+  end
+
+  def drink_params
+    params.require(:drink).permit(
+      :name,
+      :glass,
+      :category,
+      :ingredients,
+      :amounts,
+      :special_ingredients,
+      :garnish,
+      :preparation
+    )
+  end
 end

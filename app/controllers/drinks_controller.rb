414cea1badc26bb84132fe61cdae0ea2a6daa271
Cameron Irmas <cjirmas@gmail.com>
2014-12-01 16:53:16 -0800

    convert cL to oz

--- app/controllers/drinks_controller.rb
+++ app/controllers/drinks_controller.rb
@@ -7,6 +7,7 @@ class DrinksController < ApplicationController
   end
 
   def show
+    convert_to_oz(@drink)
   end
 
   def edit
@@ -31,7 +32,7 @@ class DrinksController < ApplicationController
   def list_by_ingredient
     user_ingredients = params["ingredients"].delete(".").split(", ").map { |ing| ing.strip.downcase }
 
-    @drinks = Drink.all.select do |drink|
+    @drinks = Drink.all.order(:name).select do |drink|
       drink.ingredients.all? { |ing| user_ingredients.include?(ing.downcase) }
     end
     render "list_by_ingredient"
@@ -40,13 +41,33 @@ class DrinksController < ApplicationController
   def list_cabinet
     @user_ing = params["ingredient"].delete(".").strip.downcase
 
-    @drinks = Drink.all.select do |drink|
+    @drinks = Drink.all.order(:name).select do |drink|
       drink.ingredients.map { |ing| ing.downcase }.include?(user_ing)
     end
     render "cabinet_list"
   end
 
   private
+
+  def convert_to_oz(drink)
+    drink.amounts.map! do |amt|
+      if amt.to_f >= 3
+        if amt.to_f % 3 == 0
+          "#{amt.to_i / 3}"
+        elsif amt.to_f % 1 != 0
+          "#{amt.to_i / 3} and #{(amt.to_f % 1).to_r}"
+        else
+          "#{amt.to_i / 3} and #{amt.to_i % 3}/3"
+        end
+      else
+        if amt.to_f % 1 != 0
+          "#{Rational(amt.to_f,3)}"
+        else
+          "#{amt.to_i}/3"
+        end
+      end
+    end
+  end
 
   def set_drink
     @drink = Drink.find(params[:id])
